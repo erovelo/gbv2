eaf15cd18b09541730866cdd76f2d2a5e63e3c08
Eric Rovelo <eric.1160@hotmail.com>
2019-02-17 23:11:59 -0600

    cambios multilenguaje

--- GuestBooker/GuestBooker/Pages/Main/LoginPage.xaml.cs
+++ GuestBooker/GuestBooker/Pages/Main/LoginPage.xaml.cs
@@ -10,6 +10,8 @@ namespace GuestBooker.Pages.Main
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        private bool NotAnimate = false;
+
         public LoginPage()
         {
             NavigationPage.SetHasNavigationBar(this, false);
@@ -19,19 +21,23 @@ namespace GuestBooker.Pages.Main
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            AnimationInitAsync();
+            if(!NotAnimate) AnimationInitAsync();
         }
 
         // Animacion del logo inicial
         private async Task AnimationInitAsync()
         {
-            //LogoImg.Opacity = 0;
+            NotAnimate = true;
+
+            // Show logo
             await Task.Delay(2000);
             await LogoImg.FadeTo(1, 1200, Easing.SinIn);
             
+            // Move logo to left
             var MoveLogoAnimation = new Animation(v => ColLogoSpacing.Width = new GridLength(v, GridUnitType.Star), 1, 0);
             MoveLogoAnimation.Commit(this, "MoveLogoAnimation", 16, 1000, Easing.SinInOut, (v, c) => 
             {
+                // Mostrar login
                 BgDark.FadeTo(0.5, 1200);
                 Logins.FadeTo(1, 1200);
             });
