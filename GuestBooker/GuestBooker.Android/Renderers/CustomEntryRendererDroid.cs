56d7a7cfe1994abb463051f7921c5fbd832f3e3a
Eric Rovelo <eric.1160@hotmail.com>
2019-02-18 16:17:35 -0600

    archivos faltantes

---
+++ GuestBooker/GuestBooker.Android/Renderers/CustomEntryRendererDroid.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using GuestBooker.Controls;
+using GuestBooker.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRendererDroid))]
+namespace GuestBooker.Droid.Renderers
+{
+    public class CustomEntryRendererDroid : EntryRenderer
+    {
+        public CustomEntryRendererDroid(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            var nativeControl = Control;
+
+            if (e.NewElement != null)
+            {
+                var gradientDrawable = new GradientDrawable();
+                gradientDrawable.SetCornerRadius(25f);
+                gradientDrawable.SetColor(global::Android.Graphics.Color.ParseColor("#30000000"));
+                gradientDrawable.SetStroke(1, Android.Graphics.Color.White);
+
+                Control.SetBackground(gradientDrawable);
+                Control.SetPadding(25, 4, 0, 0);
+                Control.Gravity = GravityFlags.CenterVertical;
+                //Control.TextAlignment = Android.Views.TextAlignment.Center;
+            }
+        }
+    }
+}
---
+++ GuestBooker/GuestBooker/Controls/CustomEntry.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GuestBooker.Controls
+{
+    public class CustomEntry : Entry
+    {
+        public CustomEntry()
+        {
+            // Teclado sin autocompletado o sugerencias
+            Keyboard = Keyboard.Create(KeyboardFlags.None);
+            
+            // Evento cuando cambie el texto
+            this.TextChanged += CustomEntryTextChanged;
+        }
+
+        // Convierte el texto ingresado a minusculas
+        // Si el entry es numerico, verifica que solo haya números
+        private void CustomEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Viejo valor
+            var old = e.OldTextValue;
+
+            // Si es teclado numerico solo se aceptan numeros
+            if ((Keyboard == Keyboard.Numeric || Keyboard == Keyboard.Telephone) && (!string.IsNullOrEmpty(e.NewTextValue)))
+            {
+                if (HasInvalidNumber(e.NewTextValue)) Text = e.OldTextValue;
+            }
+            else
+            {
+                // Solo se aceptan minusculas
+                Text = Text.ToLower();
+            }
+        }
+
+        // Regresa true si no es numero
+        bool HasInvalidNumber(string cad) => cad.Any(c => char.IsLetter(c) || char.IsSymbol(c) || char.IsWhiteSpace(c) || char.IsPunctuation(c));
+    }
+}
