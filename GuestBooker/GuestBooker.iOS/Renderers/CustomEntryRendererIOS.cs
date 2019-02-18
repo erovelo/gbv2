a5e56d13f6a07400377eb17bfb0a99f0e9a92778
Ercik <ercik@MacBook-Pro-Eric.local>
2019-02-18 17:00:28 -0600

    cambios en login

---
+++ GuestBooker/GuestBooker.iOS/Renderers/CustomEntryRendererIOS.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CoreGraphics;
+using GuestBooker.Controls;
+using GuestBooker.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRendererIOS))]
+namespace GuestBooker.iOS.Renderers
+{
+    public class CustomEntryRendererIOS : EntryRenderer
+    {
+        public CustomEntryRendererIOS()
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.Layer.CornerRadius = 14f;
+                Control.Layer.ShadowRadius = 14f;
+                Control.Layer.BorderWidth = 1f;
+                Control.Layer.BorderColor = Color.White.ToCGColor();
+                //Control.Layer.BackgroundColor = UIColor.FromRGBA(0, 0, 0, 50).CGColor;
+
+                Control.BackgroundColor = UIColor.FromRGBA(0, 0, 0, 50);
+                Control.TextColor = Color.White.ToUIColor();
+
+                // Padding
+                Control.LeftView = new UIView(new CGRect(0, 0, 10, 0));
+                Control.LeftViewMode = UITextFieldViewMode.Always;
+                Control.RightView = new UIView(new CGRect(0, 0, 10, 0));
+                Control.RightViewMode = UITextFieldViewMode.Always;
+            }
+        }
+    }
+}
