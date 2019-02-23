0811c571ce7f48c04ed7c4c405f95240882a923a
Ercik <ercik@MacBook-Pro-Eric.local>
2019-02-23 01:13:08 -0600

    faltantes

---
+++ GuestBooker/GuestBooker/Controls/CustomPicker.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GuestBooker.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomPicker : ContentView
+    {
+        public IList ItemsSource { get { return PickerControl.ItemsSource; } set { PickerControl.ItemsSource = value; } }
+        public BindingBase ItemDisplayBinding { get { return PickerControl.ItemDisplayBinding; } set { PickerControl.ItemDisplayBinding = value; } }
+        public object SelectedItem { get { return PickerControl.SelectedItem; } set { PickerControl.SelectedItem = value; } }
+        public int SelectedIndex { get { return PickerControl.SelectedIndex; } set { PickerControl.SelectedIndex = value; } }
+
+        public CustomPicker()
+        {
+            InitializeComponent();
+
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            //PickerControl.Focus();
+        }
+    }
+}
